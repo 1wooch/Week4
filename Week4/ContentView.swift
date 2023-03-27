0abfa6416a8e73f032ac39415ca2fe9a662821c6
wonwoo choi <onechoo1106@gmail.com>
2023-03-27 17:30:33 +1000

    Initial Commit

---
+++ Week4/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Week4
+//
+//  Created by Wonwoo Choi on 27/3/2023.
+//
+
+import SwiftUI
+
+struct Guest:Hashable{
+    var name:String="<unknown>"
+}
+
+struct ContentView: View {
+    //@State var clickTimes = 0
+    @State var myGuests=[
+    Guest(name: "Tom"),Guest(name: "Jerry")
+    ]
+    
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach(myGuests,id: \.self) {
+                    guest in
+                    /*@START_MENU_TOKEN@*/Text(guest.name)/*@END_MENU_TOKEN@*/
+                }
+            }.navigationBarItems(leading: EditButton(), trailing:Button("+"){
+                //enter what + button will do if it clicked
+                let newGuest = Guest(name: "New Guest \(myGuests.count)")
+                myGuests.append(newGuest)
+            }).navigationTitle("My Guests")
+            
+        }}
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Week4/Week4App.swift
@@ -0,0 +1,17 @@
+//
+//  Week4App.swift
+//  Week4
+//
+//  Created by Wonwoo Choi on 27/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct Week4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
